abba5e205c65233e965474fc2e1571fc5724eb3a
Mohamed Radhi Guennichi <guennichiradhi@gmail.com>
2020-09-12 01:18:03 +0100

    add homepage integration test

---
+++ tests/Integration/PageControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Depense.Net package.
+ *
+ * (c) Mohamed Radhi GUENNICHI <rg@mate.tn> <+216 50 711 816>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Depense\Tests\Integration;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testDisplaysHomepage()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
